91e9091d744c19c83a9a94af1dfaed17aac03902
hxezin <leehxejin@gmail.com>
2022-08-24 16:57:24 +0900

    Frontend initial setting

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+// import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  // const [helloWorld, setHelloWorld] = useState('');
+
+  // useEffect(() => {
+  //   fetch('http://ec2-54-86-32-163.compute-1.amazonaws.com:8080/')
+  //     .then((res) => res.text())
+  //     .then((data) => {
+  //       setHelloWorld(data);
+  //     });
+  // });
+
+  return <div className="App">HOME</div>;
+}
+
+export default App;
