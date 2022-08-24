70a9e221e41bf52b90cef77ccd9b7ce530aa4ae2
kwangsooPark <steven@spesys.co.kr>
2022-08-24 16:15:25 +0900

    Initial Server -> fixed

---
+++ backend/src/main/java/com/codestates/pre31/Pre31Application.java
@@ -0,0 +1,13 @@
+package com.codestates.pre31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pre31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pre31Application.class, args);
+	}
+
+}
