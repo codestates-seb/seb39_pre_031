9bc0c9a432eef7dc54afeca8fda08e1975e0342b
hxezin <leehxejin@gmail.com>
2022-08-30 20:54:37 +0900

    refactor: 라우팅 수정

---
+++ frontend/src/SplitLayout.js
@@ -0,0 +1,31 @@
+import { Outlet } from 'react-router-dom';
+import LeftSide from './components/Side/LeftSide';
+import styled from 'styled-components';
+import RightSide from './components/Side/RightSide';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
+const Content = styled.div`
+  height: 100%;
+  width: 70%;
+  display: flex;
+  padding: 24px;
+`;
+
+const SplitLayout = () => {
+  return (
+    <Container>
+      <LeftSide />
+      <Content>
+        <Outlet />
+        <RightSide />
+      </Content>
+    </Container>
+  );
+};
+
+export default SplitLayout;
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,32 +1,43 @@
-import { Outlet } from 'react-router-dom';
-import LeftSide from '../../components/Side/LeftSide';
 import styled from 'styled-components';
-import RightSide from '../../components/Side/RightSide';
-import HomeContainer from './HomeContainer';
+import AskQuestionBtn from '../../components/ask/AskQuestionBtn';
+import QuestionList from '../../components/question/QuestionList';
+import QuestionTab from '../../components/question/QuestionTab';
 
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
+const Page = styled.div`
   display: flex;
-  justify-content: center;
-`;
-const Content = styled.div`
-  height: 100%;
-  width: 70%;
-  display: flex;
+  flex-direction: column;
   padding: 24px;
+  width: 80%;
+`;
+
+const TopContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Headline = styled.div`
+  font-size: 1.5rem;
+  margin-bottom: 24px;
+`;
+
+const MidContainer = styled.div`
+  margin-bottom: 12px;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 const Home = () => {
   return (
-    <Container>
-      <LeftSide />
-      <Content>
-        <Outlet />
-        <HomeContainer />
-        <RightSide />
-      </Content>
-    </Container>
+    <Page>
+      <TopContainer>
+        <Headline>Top Questions</Headline>
+        <AskQuestionBtn />
+      </TopContainer>
+      <MidContainer>
+        <QuestionTab />
+      </MidContainer>
+      <QuestionList />
+    </Page>
   );
 };
 
