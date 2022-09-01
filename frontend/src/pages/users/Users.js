2df89ba24cfcb57f2fe7068584522337d099f398
hxezin <102649010+hxezin@users.noreply.github.com>
2022-09-01 15:44:56 +0900

    feat: Pagination UI 구현 (#25)

--- frontend/src/pages/users/Users.js
+++ frontend/src/pages/users/Users.js
@@ -3,6 +3,7 @@ import UserFilter from '../../components/user/UserFilter';
 import UserTab from '../../components/user/UserTab';
 import UserList from '../../components/user/UserList';
 import UserSubTab from '../../components/user/UserSubTab';
+import Paging from '../../common/Paging';
 
 const Container = styled.div`
   width: 100%;
@@ -35,6 +36,12 @@ const TabBlock = styled.div`
   margin-left: auto !important;
 `;
 
+const Pagination = styled.div`
+  margin: 20px 0;
+  display: flex;
+  justify-content: flex-end;
+`;
+
 const Users = () => {
   return (
     <Container>
@@ -51,6 +58,9 @@ const Users = () => {
         <UserSubTab />
       </SubContainer>
       <UserList />
+      <Pagination>
+        <Paging />
+      </Pagination>
     </Container>
   );
 };
