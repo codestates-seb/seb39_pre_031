59cc6ca14edb2347293741f0447ac9d4d303d91b
Shin-Dong-Jae <101863629+lIIIlIIIlIIIl@users.noreply.github.com>
2022-09-01 13:33:46 +0900

    [FE] Feat/edit (#22)
    * feat: edit component 기능 구현
    
    * fix: url 이동경로 변경
    
    * refactor: container css
    
    * fix: rout
    
    Co-authored-by: hxezin <102649010+hxezin@users.noreply.github.com>

--- frontend/src/RoutesTree.js
+++ frontend/src/RoutesTree.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 import { Route, Routes } from 'react-router-dom';
 import SplitLayout from './SplitLayout';
 import Login from './pages/login/login';
@@ -8,6 +9,7 @@ import Users from './pages/users/Users';
 import Ask from './pages/questions/Ask';
 import Home from './pages/home/Home';
 import QuestionDetail from './pages/questions/QuestionDetail';
+import Edit from './pages/questions/Edit';
 import Password from './pages/users/Password';
 
 const RoutesTree = () => {
@@ -23,6 +25,7 @@ const RoutesTree = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/questions/ask" element={<Ask />} />
+      <Route path="/questions/:questionId/edit" element={<Edit />} />
       <Route path="/password" element={<Password />} />
     </Routes>
   );
---
+++ frontend/src/components/Edit/EditComponent.js
@@ -0,0 +1,116 @@
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import styled from 'styled-components';
+import Card from '../../common/Card';
+import Input from '../../common/Input';
+import { useEffect, useRef, useState } from 'react';
+import SubmitButton from '../../common/Button/SubmitButton';
+
+const Container = styled.div`
+  width: 70%;
+  margin: 0 auto;
+`;
+
+const Label = styled.label`
+  width: 100%;
+  font-size: 0.9rem;
+  font-weight: 700;
+  cursor: pointer;
+
+  div {
+    font-size: 0.7rem;
+    font-weight: 400;
+    margin-bottom: 5px;
+  }
+`;
+
+const PreviewContainer = styled.div`
+  margin: 1rem 0;
+
+  div {
+    font-size: 12px;
+    color: hsl(210, 8%, 45%);
+    margin-top: 10px;
+  }
+
+  code {
+    color: black;
+    padding: 2px 4px;
+    border: 1px solid black;
+    border-radius: 3px;
+    background-color: hsl(210, 8%, 95%);
+  }
+`;
+
+const EditComponent = ({ editData }) => {
+  const [title, setTitle] = useState(editData.title);
+  const editorRef = useRef(null);
+
+  const onChageHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  useEffect(() => {
+    editorRef.current?.getInstance().setMarkdown(editData.body);
+  }, []);
+
+  return (
+    <Container>
+      <Card width="842px" margin="0 0 1rem 0">
+        <Label>
+          Title
+          <div>
+            Be specific and imagine you’re asking a question to another person
+          </div>
+        </Label>
+        <Input
+          value={title}
+          onChange={onChageHandler}
+          placeholder="e.g. Is there an R function for finding the index an element in a vector?"
+        />
+        <Label>
+          Body
+          <div>
+            Include all the information someone would need to answer your
+            question
+          </div>
+        </Label>
+        <Editor
+          ref={editorRef}
+          hideModeSwitch="true"
+          height="250px"
+          initialEditType="markdown"
+          toolbarItems={[
+            ['bold', 'italic'],
+            ['link', 'quote', 'code', 'image', 'codeblock'],
+            ['ol', 'ul', 'heading', 'hr'],
+          ]}
+        ></Editor>
+        <PreviewContainer>
+          <div>
+            ``` <code>code</code> ``` <strong>**bold**</strong> *<em>italic</em>
+            * &gt;quote
+          </div>
+        </PreviewContainer>
+        <Label className="tags">
+          Tags
+          <div>Add up to 5 tags to describe what your question is about</div>
+        </Label>
+        <Input placeholder="e.g. Is there an R function for finding the index an element in a vector?" />
+      </Card>
+      <SubmitButton
+        go={`/questions/${editData.id}`}
+        type="submit"
+        btnName="Edit your question"
+        width="150px"
+        height="35px"
+        color="white"
+      ></SubmitButton>
+    </Container>
+  );
+};
+
+// TODO 버튼을 클릭하고 나면 뒤로가기 못하게 막기
+
+export default EditComponent;
