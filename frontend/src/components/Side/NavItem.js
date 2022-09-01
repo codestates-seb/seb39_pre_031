dfc67775de5318271b6ea39df90de59e82ce0e6a
hxezin <leehxejin@gmail.com>
2022-09-01 11:31:46 +0900

    refactor: 왼쪽 sidebar css 수정 및 NavLink 적용

--- frontend/src/components/Side/NavItem.js
+++ frontend/src/components/Side/NavItem.js
@@ -1,57 +1,56 @@
 /* eslint-disable react/prop-types */
-import { Link } from 'react-router-dom';
-import styled, { css } from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import { ImEarth } from 'react-icons/im';
 
-const NavList = styled.li`
-  width: ${({ width }) => (width ? width : '139px')};
-  height: ${({ height }) => (height ? height : '33px')};
-  font-size: ${({ fontSize }) => (fontSize ? fontSize : '0.8rem')};
-  color: ${({ color }) => (color ? color : 'hsl(210, 8%, 25%)')};
-  padding: ${({ padding }) => (padding ? padding : '0 0 0 29px')};
-  cursor: ${({ cursor }) => (cursor ? cursor : 'pointer')};
+const NavList = styled(NavLink)`
+  display: list-item;
+  width: 100%;
+  height: 30px;
+  line-height: 2;
+  font-size: 13px;
+  padding: ${({ padding }) => (padding ? padding : '4px 4px 4px 30px')};
   display: flex;
   align-items: center;
-  pointer-events: ${({ pointerEvents }) =>
-    pointerEvents ? pointerEvents : 'auto'};
-
-  .icon {
-    font-size: 14px;
-    margin-right: 0.3rem;
-  }
 
   &:hover {
     color: black;
   }
 
-  ${(props) =>
-    props.isClick &&
-    css`
-      color: black;
-      width: ${({ width }) => (width ? width : '132px')};
-      background-color: hsl(210, 8%, 95%);
-      border-right: 4px solid #f48224;
-      font-weight: 700;
+  &.active {
+    color: black;
+    background-color: hsl(210, 8%, 95%);
+    border-right: 4px solid #f48224;
+    font-weight: 700;
+  }
+`;
 
-      &:hover {
-        font-weight: 700;
-      }
-    `}
+const Text = styled.div`
+  color: hsl(210, 8%, 35%);
+
+  .icon {
+    font-size: 14px;
+    margin-left: 2px;
+    margin-right: 6px;
+  }
+`;
+
+const Public = styled.div`
+  color: hsl(210, 8%, 45%);
+  font-size: 11px;
+  margin: 16px 0 6px 8px;
 `;
 
 const NavItem = ({ path, text, ...props }) => {
   return text === 'PUBLIC' ? (
-    <NavList {...props}>
-      {text === 'Questions' && <ImEarth className="icon" />}
-      {text}
-    </NavList>
+    <Public>{text}</Public>
   ) : (
-    <Link to={path}>
-      <NavList {...props}>
+    <NavList to={path} {...props}>
+      <Text>
         {text === 'Questions' && <ImEarth className="icon" />}
         {text}
-      </NavList>
-    </Link>
+      </Text>
+    </NavList>
   );
 };
 
