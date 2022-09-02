6d96d0922d3eab3e4eb4ce99f7482f86275b0a66
shindongjae <sdhsdj6450@gmail.com>
2022-09-02 16:07:41 +0900

    api setting

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -9,3 +9,13 @@ export const signupApi = async (body) => {
   const url = '/members';
   return axios.post(url, body);
 };
+
+export const homeApi = async (body) => {
+  const url = '/home';
+  return axios.get(url, body);
+};
+
+export const questionApi = async (body) => {
+  const url = `/questions`;
+  return axios.get(url, body);
+};
