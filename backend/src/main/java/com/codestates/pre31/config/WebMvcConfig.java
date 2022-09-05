8df5ef9a77326385cc57217d3148e89e4e2ad374
minoolian <mw.lee@kakao.com>
2022-09-05 17:20:07 +0900

    test build

--- backend/src/main/java/com/codestates/pre31/config/WebMvcConfig.java
+++ backend/src/main/java/com/codestates/pre31/config/WebMvcConfig.java
@@ -10,6 +10,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://seb39-pre-031.vercel.app");
+                .allowedOrigins("http://localhost:3000");
     }
 }
