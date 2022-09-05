1ceca56ba5b4f7b35036f7165b041eec21b9f2f8
minoolian <mw.lee@kakao.com>
2022-09-05 17:22:25 +0900

    build test

--- backend/src/main/java/com/codestates/pre31/user/config/security/CorsConfig.java
+++ backend/src/main/java/com/codestates/pre31/user/config/security/CorsConfig.java
@@ -17,6 +17,7 @@ public class CorsConfig {
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
